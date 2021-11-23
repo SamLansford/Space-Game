d401c4aab08150c63d220c1cbf69e82aaee9af2f
Sam Lansford <75640963+SamLansford@users.noreply.github.com>
2021-11-23 18:16:15 +0000

    Loop the rocks to fall over and over again

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 FPS = 60
 WIDTH = 500
-HIGHT = 500
+HEIGHT = 500
 
 WHITE = (255,255,255)
 GREEN = (0, 255, 0)
@@ -12,7 +12,7 @@ RED = (255, 0, 0)
 
 #Game initialization and creating Windows
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Game")
 clock = pygame.time.Clock()
 
@@ -23,7 +23,7 @@ class Player(pygame.sprite.Sprite):
       self.image.fill(GREEN)
       self.rect = self.image.get_rect()
       self.rect.centerx = WIDTH / 2
-      self.rect.bottom = HIGHT - 10
+      self.rect.bottom = HEIGHT - 10
       self.speedx = 8
 
   def update(self):
@@ -46,10 +46,16 @@ class Rock(pygame.sprite.Sprite):
       self.rect = self.image.get_rect()
       self.rect.x = random.randrange(0, WIDTH - self.rect.width)
       self.rect.y = random.randrange(-100, -40) 
-      self.speedy = 2
+      self.speedy = random.randrange(2, 10)
+      self.speedx = random.randrange(-3, 3)
 
   def update(self):
       self.rect.y += self.speedy
+      self.rect.x += self.speedx
+      if self.rect.top > HEIGHT:
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(2, 10)
+        self.speedx = random.randrange(-3, 3)        
 
 
 all_sprites = pygame.sprite.Group()

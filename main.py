7840fd438aa1e87ff6b257c37c0983712d8d5ef8
Sam Lansford <75640963+SamLansford@users.noreply.github.com>
2021-11-23 16:04:43 +0000

    Add random falling object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #sprite
 import pygame
+import random
 
 FPS = 60
 WIDTH = 500
@@ -7,6 +8,7 @@ HIGHT = 500
 
 WHITE = (255,255,255)
 GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 #Game initialization and creating Windows
 pygame.init()
@@ -42,26 +44,19 @@ class Rock(pygame.sprite.Sprite):
       self.image = pygame.Surface((30, 40))
       self.image.fill(RED)
       self.rect = self.image.get_rect()
-      self.rect.centerx = WIDTH / 2
-      self.rect.bottom = HIGHT - 10
-      self.speedx = 8
+      self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+      self.rect.y = random.randrange(-100, -40) 
+      self.speedy = 2
 
   def update(self):
-      key_pressed = pygame.key.get_pressed()
-      if key_pressed[pygame.K_d]:
-        self.rect.x += self.speedx
-      if key_pressed[pygame.K_a]:
-        self.rect.x -= self.speedx 
-
-      if self.rect.right > WIDTH:
-        self.rect.right = WIDTH
-      if self.rect.left < 0:
-        self.rect.left = 0
+      self.rect.y += self.speedy
 
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+rock = Rock()
+all_sprites.add(rock) 
 
 #game loop
 running  = True

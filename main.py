537c75fc6da9f23ce613e16c1cfb2261c300b379
Sam Lansford <75640963+SamLansford@users.noreply.github.com>
2021-11-27 22:23:05 +0000

    Feat: add rotation to the rocks and center the rotations so it will not look weird

--- main.py
+++ main.py
@@ -60,8 +60,9 @@ class Player(pygame.sprite.Sprite):
 class Rock(pygame.sprite.Sprite):
   def __init__(self):
       pygame.sprite.Sprite.__init__(self)
-      self.image = rock_img
-      self.image.set_colorkey(BLACK)
+      self.image_original = rock_img
+      self.image_original.set_colorkey(BLACK)
+      self.image = self.image_original.copy()
       self.rect = self.image.get_rect()
       self.radius = self.rect.width * 0.85 / 2
       #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
@@ -69,8 +70,20 @@ class Rock(pygame.sprite.Sprite):
       self.rect.y = random.randrange(-100, -40) 
       self.speedy = random.randrange(3, 8)
       self.speedx = random.randrange(-3, 3)
+      self.total_degree = 0
+      self.rot_degree = random.randrange(-3, 3)
+
+  def rotate(self):
+      self.total_degree += self.rot_degree
+      self.total_degree = self.total_degree % 360
+      self.image = pygame.transform.rotate(self.image_original, self.total_degree)
+      #81-83 ensures the roations doesn't look twitchy when the rocks rotates
+      center = self.rect.center
+      self.rect = self.image.get_rect()
+      self.rect.center = center
 
   def update(self):
+      self.rotate()
       self.rect.y += self.speedy
       self.rect.x += self.speedx
       if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:

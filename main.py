9d956c6d57fb9fe7639766292bff968747015638
Sam Lansford <75640963+SamLansford@users.noreply.github.com>
2021-11-27 01:22:23 +0000

    Feat: changing the collision shape from square to circle, so it's more accurate

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ class Player(pygame.sprite.Sprite):
       self.image = pygame.transform.scale(player_img, (50, 38))
       self.image.set_colorkey(BLACK)
       self.rect = self.image.get_rect()
+      self.radius = 20
+      # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
       self.rect.centerx = WIDTH / 2
       self.rect.bottom = HEIGHT - 10
       self.speedx = 8
@@ -61,6 +63,8 @@ class Rock(pygame.sprite.Sprite):
       self.image = rock_img
       self.image.set_colorkey(BLACK)
       self.rect = self.image.get_rect()
+      self.radius = self.rect.width * 0.85 / 2
+      #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
       self.rect.x = random.randrange(0, WIDTH - self.rect.width)
       self.rect.y = random.randrange(-100, -40) 
       self.speedy = random.randrange(3, 8)
@@ -122,7 +126,7 @@ while running:
       all_sprites.add(r)
       rocks.add(r)
 
-  hits = pygame.sprite.spritecollide(player, rocks, False)
+  hits = pygame.sprite.spritecollide(player, rocks, False, pygame.sprite.collide_circle)
   if hits:
       running = False
 

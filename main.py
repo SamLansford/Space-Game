b591b5d24cd50ce22cdee0b736d3fb2006a06718
Sam Lansford <75640963+SamLansford@users.noreply.github.com>
2021-12-01 04:59:20 +0000

    Feat: added health bar for player and function to remove health depending on the size of the rock

--- main.py
+++ main.py
@@ -49,6 +49,22 @@ def draw_text(surf, text, size, x, y):
     text_rect.top = y
     surf.blit(text_surface, text_rect)
 
+def new_rock():
+    r = Rock()
+    all_sprites.add(r)
+    rocks.add(r)
+
+def draw_health(surf, hp, x, y):
+    if hp < 0:
+        hp = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (hp / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT) 
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+
 class Player(pygame.sprite.Sprite):
   def __init__(self):
       pygame.sprite.Sprite.__init__(self)
@@ -60,6 +76,7 @@ class Player(pygame.sprite.Sprite):
       self.rect.centerx = WIDTH / 2
       self.rect.bottom = HEIGHT - 10
       self.speedx = 8
+      self.health = 100
 
   def update(self):
       key_pressed = pygame.key.get_pressed()
@@ -137,9 +154,7 @@ bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 for i in range(8):
-    r = Rock()
-    all_sprites.add(r)
-    rocks.add(r)
+    new_rock()
 score = 0
 pygame.mixer.music.play(-1)
 
@@ -163,19 +178,21 @@ while running:
   for hit in hits:
       random.choice(expl_sounds).play()
       score += hit.radius
-      r = Rock()
-      all_sprites.add(r)
-      rocks.add(r)
+      new_rock()
 
-  hits = pygame.sprite.spritecollide(player, rocks, False, pygame.sprite.collide_circle)
-  if hits:
-      running = False
+  hits = pygame.sprite.spritecollide(player, rocks, True, pygame.sprite.collide_circle)
+  for hit in hits:
+      new_rock()
+      player.health -= hit.radius
+      if player.health <= 0:
+        running = False
 
   #display screen
   screen.fill(BLACK)
   screen.blit(background_img, (0,0))
   all_sprites.draw(screen)
   draw_text(screen, str(score), 18, WIDTH / 2, 10)
+  draw_health(screen, player.health, 5, 15)
   pygame.display.update()
 
 pygame.quit()
